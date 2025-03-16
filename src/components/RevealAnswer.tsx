071323fedc2b8d663e173da1f56e53e1c9db98d1
Ketsia Lumiere Donfack Ouwe <ketsiad.udel.edu>
2025-03-16 03:42:16 -0400

    reveal answer done

--- src/components/RevealAnswer.tsx
+++ src/components/RevealAnswer.tsx
@@ -2,5 +2,18 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function RevealAnswer(): React.JSX.Element {
-    return <div>Reveal Answer</div>;
+    const [see, setVisible] = useState<boolean>(false);
+
+    return (
+        <div>
+            <Button
+                onClick={() => {
+                    setVisible(!see);
+                }}
+            >
+                Reveal Answer
+            </Button>
+            {see && <div>42</div>}
+        </div>
+    );
 }
